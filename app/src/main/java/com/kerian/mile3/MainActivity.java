865e6e6ecb7492f89183f759ce0d9c406e42a089
kerianVaraine <chambervulgarus@gmail.com>
2019-10-02 11:14:44 +1300

    all the things

--- app/src/main/java/com/kerian/mile3/MainActivity.java
+++ app/src/main/java/com/kerian/mile3/MainActivity.java
@@ -1,11 +1,15 @@
 package com.kerian.mile3;
 
+import androidx.annotation.DrawableRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.estimote.mustard.rx_goodness.rx_requirements_wizard.Requirement;
@@ -16,12 +20,12 @@ import com.estimote.proximity_sdk.api.ProximityObserverBuilder;
 import com.estimote.proximity_sdk.api.ProximityZone;
 import com.estimote.proximity_sdk.api.ProximityZoneBuilder;
 import com.estimote.proximity_sdk.api.ProximityZoneContext;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import kotlin.Unit;
 import kotlin.jvm.functions.Function0;
@@ -31,10 +35,27 @@ public class MainActivity extends AppCompatActivity {
 
     private ProximityObserver proximityObserver;
 
-    private TextView SensorNumber;
+    private TextView SensorNumber, sectorLetter;
     private Button scanningButton;
     private boolean isScanning = false;
+    private ImageView player1;
+    private ImageView player2;
+    private boolean player2isPlaying = false;
 
+    //timer things for replay
+    long startTime;
+    long delay;
+    Timer p2Timer = new Timer();
+    private Button toggleP2;
+
+        //arraylist to be populate by array of noteObject class
+    ArrayList<NoteObject> taskList = new ArrayList();
+
+    //
+
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,9 +63,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //layout stuff
         SensorNumber = findViewById(R.id.SectorNumber);
-
+        sectorLetter = findViewById(R.id.scoreObj);
         scanningButton = findViewById(R.id.toggleScanning);
 
+        //image things
+        player1 = findViewById(R.id.Player1);
+        player2 = findViewById(R.id.Player2);
+
+        //
+        startTime = System.currentTimeMillis();
+        toggleP2 = findViewById(R.id.toggleP2);
 
         //
 
@@ -69,25 +97,29 @@ public class MainActivity extends AppCompatActivity {
 
                         @Override
                     public Unit invoke(Set<? extends ProximityZoneContext> contexts) {
-
-                        //
-                        //IF SCANNING ON!!!!!
-                        //
+                            if(isScanning) {
                                 List<String> sectors = new ArrayList<>();
-                            for (ProximityZoneContext context : contexts) {
-                                sectors.add(context.getAttachments().get("sector"));
-                            }
+                                for (ProximityZoneContext context : contexts) {
+                                    sectors.add(context.getAttachments().get("sector"));
+                                }
 
-                            Log.d("app", "In range of sectors: " + sectors);
-                            SensorNumber.setText(getResources().getString(R.string.SectorNumber, sectors.toString()));
-//
+                                Log.d("app", "In range of sectors: " + sectors);
+                                SensorNumber.setText(getResources().getString(R.string.SectorNumber, sectors.toString()));
+
+                                //this be the meat
+                                sectorCombo(sectors);
+                                //
+
+                                //Log.d("app", "delay time: " + delay);
+
+
 //                            //database Stuff
-                            //Uncomment to send to database.
+                                //Uncomment to send to database.
 
 //                            FirebaseDatabase database = FirebaseDatabase.getInstance();
 //                            DatabaseReference myRef = database.getReference("P1");
 //                            myRef.setValue(sectors);
-                            
+                            }
                                 return null;
                     }
                 }).build();
@@ -123,10 +155,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
     @Override
     protected void onResume() {
         super.onResume();
-
+        isScanning = false;
+//        if(player2isPlaying) {
+//            player2Play();
+//        }
     }
 
     public void buttonClick(View view){
@@ -136,17 +172,162 @@ public class MainActivity extends AppCompatActivity {
                     isScanning = true;
                     scanningButton.setText(getResources().getString(R.string.stopScanning));
                     System.out.println("on");
-
-                    break;
                 }else {
                     isScanning = false;
                     scanningButton.setText(getResources().getString(R.string.startScanning));
                     System.out.println("off");
-                    break;
                 }
+                break;
+            case R.id.toggleP2:
+                if(!player2isPlaying){
+                    player2isPlaying = true;
+                    toggleP2.setText(R.string.stopP2);
+                } else {
+                    player2isPlaying = false;
+                    toggleP2.setText(R.string.startP2);
+
+                }
+                break;
         }
     }
 
+    //
+    //piece logic
+    //
+
+    private void sectorCombo(List<String> sectors) {
+        String sectorArea;
+        //int notes; //gets resID for svg.
+        int notes = 0; //gets resID for svg.
+
+        boolean[] s = {sectors.contains("1"), sectors.contains("2"), sectors.contains("3"), sectors.contains("4")};
+//convert to switch statement, set a local var to string of sector and drawable, extract delay times from here.
+        if(s[0] && !s[1] && !s[2] && !s[3]){
+            sectorArea = "a";
+            notes = R.drawable.sc_1;
+        } else if(s[0] && s[1] && !s[2] && !s[3]){
+            sectorArea = "b";
+            notes = R.drawable.sc_2;
+        } else if(!s[0] && s[1] && !s[2] && !s[3]){
+            sectorArea = "c";
+            notes = R.drawable.sc_3;
+        }  else if(!s[0] && s[1] && s[2] && !s[3]) {
+            sectorArea = "d";
+            notes = R.drawable.sc_4;
+        }  else if(!s[0] && !s[1] && s[2] && !s[3]) {
+            sectorArea = "e";
+            notes = R.drawable.sc_5;
+        }  else if(!s[0] && !s[1] && s[2] && s[3]){
+            sectorArea = "f";
+            notes = R.drawable.sc_6;
+        } else if(!s[0] && !s[1] && !s[2] && s[3]) {
+            sectorArea = "g";
+            notes = R.drawable.sc_7;
+        } else if(!s[0] && s[1] && s[2] && s[3]) {
+            sectorArea = "h";
+            notes = R.drawable.sc_8;
+        } else if(s[0] && !s[1] && s[2] && s[3]) {
+            sectorArea = "i";
+            notes = R.drawable.sc_9;
+        } else if(s[0] && s[1] && !s[2] && s[3]) {
+            sectorArea = "j";
+            notes = R.drawable.sc_10;
+        } else if(s[0] && s[1] && s[2] && !s[3]){
+            sectorArea = "l";
+            notes = R.drawable.sc_11;
+        } else if(!s[0] && !s[1] && !s[2] && !s[3]) {
+            sectorArea = "all off";
+        } else if(s[0] && s[1] && s[2] && s[3]){
+            sectorArea = "all on";
+        } else {
+            sectorArea = "undefined";
+            notes = 0;
+        }
+
+        sectorLetter.setText(sectorArea);
+        player1.setImageResource(notes);
+
+        //Player 2 timer playback things
+        delay = System.currentTimeMillis() - startTime;
+        startTime = System.currentTimeMillis();
+
+        //Need to go through this for player 2; from start of piece to end of piece timer??
+        taskList.add(new NoteObject(notes, delay));
+    }
+
+
+    private void player2Play() {
+
+            if(!taskList.isEmpty()) {
+                player2isPlaying = false;
+                Log.d("app", "triggered P2-");
+                //timer event off task list, event ends with setting player2On to true, so next event can be timered
+                DelayTasks p2NextNote = new DelayTasks();
+                p2NextNote.setDelayTask(taskList.get(0).getSvg());
+                //
+                p2Timer.schedule(p2NextNote, taskList.get(0).getDelay());
+
+        }
+
+    }
+
+    /////////////
+// delayTask class to run with timer instance for the sector code.
+//To be used along with delay time, calculated in sectorCombo method to repeat previous events in player2's image view
+    class DelayTasks extends TimerTask {
+        int scoreObj;
+
+        public void setDelayTask(int task) {
+            this.scoreObj = task;
+        }
+
+        public int getDelayTask(){
+            return scoreObj;
+        }
+
+        @Override
+        public void run() {
+            //this is where the sector event gets passed to the timer object for player2 imageView, after a delay
+            //do stuff!
+            player2.setImageResource(scoreObj);
+            player2isPlaying = true;
+            Log.d("app", "P2 End:");
+        }
+    }
+
+
+    //end of main
+}
+
+
+
+//////
+//note Object, contains svg note int and delay time
+//////
+class NoteObject {
+    int svg;
+    long delay;
+
+    public NoteObject (int svg, long delay){
+        setSvg(svg);
+        setDelay(delay);
+    }
+
+    public void setSvg(int svg) {
+        this.svg = svg;
+    }
+
+    public int getSvg() {
+        return svg;
+    }
+
+    public void setDelay(long delay) {
+        this.delay = delay;
+    }
+
+    public long getDelay() {
+        return delay;
+    }
 }
 
 
@@ -178,27 +359,4 @@ public class MainActivity extends AppCompatActivity {
                         }
                 )
                 .build();
-*/
-
-/*
-                                //Converts from digit to room; my house.
-                                String s = context.getAttachments().get("sector");
-                                String room;
-                                switch(s) {
-                                    case ("1"):
-                                        room = "K's Computer";
-                                        break;
-                                    case("2"):
-                                        room = "Hallway";
-                                        break;
-                                    case("3"):
-                                        room = "K's Room";
-                                        break;
-                                    case("4"):
-                                        room = "Tui's Room";
-                                        break;
-                                    default:
-                                        room = "none";
-                                }
-                                sectors.add(room);
 */
